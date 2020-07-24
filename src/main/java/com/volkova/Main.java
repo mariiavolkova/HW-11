c0fe123a74d773d77cb39333bfc35373c5963c5c
mariiavolkova <67053060+mariiavolkova@users.noreply.github.com>
2020-07-24 18:25:38 +0300

    Reformat Code

--- src/main/java/com/volkova/Main.java
+++ src/main/java/com/volkova/Main.java
@@ -1,8 +1,10 @@
 package main.java.com.volkova;
 
+import main.java.com.volkova.app.AppDead;
+
 public class Main {
     public static void main(String[] args) {
-    AppDead dead = new AppDead();
-    dead.killMemory();
+        AppDead dead = new AppDead();
+        dead.killMemory();
     }
 }
---
+++ src/main/java/com/volkova/app/AppDead.java
@@ -0,0 +1,13 @@
+package main.java.com.volkova.app;
+
+public class AppDead {
+    public void killMemory() {
+        int[] array;
+        array = new int[1000000000];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = ((int) (Math.random() * 100000000));
+
+            System.out.println(array[i]);
+        }
+    }
+}
